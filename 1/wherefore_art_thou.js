30b75fb1e2e7d0badb46a20ab5cd1ac7a3ce7448
pybalt <leobravolbb2002bej@gmail.com>
2022-10-28 12:41:56 -0300

    format: adding to js conventions

--- 1/wherefore_art_thou.js
+++ 1/wherefore_art_thou.js
@@ -1,46 +1,32 @@
-
 function whatIsInAName(collection, source) {
-    let startTime = performance.now()
-    let arr = [];
-    const keys = Object.keys(source)
-    const values = Object.values(source)
-    let hadTheValues = [] 
-    collection.forEach(
-      function(element)
-      {
-        let elementMatchesTheValues = []
-        values.forEach(
-          function(value, index){
-            let elementValue = element[keys[index]]
-            let matchesValue = elementValue === value //boolean
-            elementMatchesTheValues.push(matchesValue)
-          }
-        )
-        hadTheValues.push(elementMatchesTheValues)
-      }
-    )
-    hadTheValues = hadTheValues.map(el => el.every(k => k===true))
-    hadTheValues.forEach(
-      function(value, index)
-      {
-          value ? arr.push(collection[index]) : undefined
-      }
-    )
-    let endTime = performance.now()
-    console.log("Performance: " + (endTime - startTime))
-    return arr
-  }
-  
-  whatIsInAName(
-    
-    [
-        { "apple": 1, "bat": 2 }, { "bat": 2 }, 
-        { "apple": 1, "bat": 2, "cookie": 2 }
-    ], 
-    
-    { "apple": 1, "bat": 2 }
-    
-    );
+  let startTime = performance.now();
+  let arr = [];
+  const keys = Object.keys(source);
+  const values = Object.values(source);
+  let hadTheValues = [];
+  collection.forEach(function (element) {
+    let elementMatchesTheValues = [];
+    values.forEach(function (value, index) {
+      let elementValue = element[keys[index]];
+      let matchesValue = elementValue === value; //boolean
+      elementMatchesTheValues.push(matchesValue);
+    });
+    hadTheValues.push(elementMatchesTheValues);
+  });
+  hadTheValues = hadTheValues.map((el) => el.every((k) => k === true));
+  hadTheValues.forEach(function (value, index) {
+    value ? arr.push(collection[index]) : undefined;
+  });
+  let endTime = performance.now();
+  console.log("Performance: " + (endTime - startTime));
+  return arr;
+}
+
+whatIsInAName(
+  [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+
+  { apple: 1, bat: 2 }
+);
 
 /*
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) should return [{ first: "Tybalt", last: "Capulet" }].
